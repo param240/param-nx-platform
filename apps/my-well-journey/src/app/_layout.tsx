a948cafdba7c6729da153362a683e2a6bb2b7fd7
Parameshwar Ande <param240@gmail.com>
2026-07-09 19:06:35 +0530

    Add debug log for trivial update in RootLayoutNav component

--- apps/my-well-journey/src/app/_layout.tsx
+++ apps/my-well-journey/src/app/_layout.tsx
@@ -8,6 +8,7 @@ import { Stack } from 'expo-router';
 
 function RootLayoutNav() {
   const { isSignedIn, isLoaded } = useAuth();
+  console.log('Test Trivial update->1.0.0')
   console.log('isSignedIn', isSignedIn);
 
   // Keep splash screen until Clerk has restored the session
